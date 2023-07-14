2ac158807306cac234241ee8798851767d4a75e2
ASTRO <anton.zorin.2002@mail.ru>
2023-07-14 19:35:32 +0500

    Initial commit

---
+++ src/main/java/koschei/models/Rabbit4.java
@@ -0,0 +1,18 @@
+package koschei.models;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Rabbit4 {
+
+    private final Duck5 duck;
+
+    public Rabbit4(Duck5 duck) {
+        this.duck = duck;
+    }
+
+    @Override
+    public String toString() {
+        return ", в зайце утка " + duck.toString();
+    }
+}
